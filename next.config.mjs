08ffcb200876ade690495eaee54f9d28d07aeb30
yeojini <yesjin1209@gmail.com>
2024-06-17 13:46:44 +0900

    config 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
+
+const debug = process.env.NODE_ENV !== 'production';
+
 const nextConfig = {
-  // enables static exports
-  output: "export"
+  reactStrictMode: true,
+  assetPrefix: !debug ? `/` : "",
 };
 
 export default nextConfig;
